4a8e642cfcdfbcdf644d841ae39d518e7d8576c8
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-02 16:14:47 -0600

    added css to navbar js component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,14 +3,24 @@ import { menuItems } from "./MenuItems";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">
           React<i className="fab fa-react"></i>
         </h1>
-        <div className="menu-icon"></div>
-        <ul>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu acive" : "nav-menu"}>
           {menuItems.map((item, index) => {
             return (
               <li key={index}>
