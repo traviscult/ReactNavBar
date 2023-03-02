8ab693f7dfca84f79fca91f319c2e8f1486b6b31
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-02 15:47:14 -0600

    imported font awesome and googlefonts

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { menuItems } from "./MenuItems";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">
+          React<i className="fab fa-react"></i>
+        </h1>
+        <div className="menu-icon"></div>
+        <ul>
+          {menuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
