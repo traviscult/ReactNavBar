47ad9d442ac18b8f4113e8f108dc14bb9a7b6b62
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-02 15:50:25 -0600

    styled navbar and imported it to app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { menuItems } from "./MenuItems";
+import "./Navbar.css";
 
 class Navbar extends Component {
   render() {
